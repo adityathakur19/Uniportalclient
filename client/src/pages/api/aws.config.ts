e9d7bdc208ffa1e14b49a4333e0b6689e2236732
Aditya Thakur <adityathakur19200@gmail.com>
2025-05-05 19:07:25 +0530

    Remove .env file containing secrets

--- client/src/pages/api/aws.config.ts
+++
@@ -1,8 +0,0 @@
-const awsConfig = {
-  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-  s3Bucket: process.env.AWS_S3_BUCKET,
-  region: process.env.AWS_REGION || 'us-east-1',
-};
-
-export default awsConfig;
