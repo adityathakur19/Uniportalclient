6478aefc423538712fa48bb9502ce13d1fb4d09d
ICHIGO su <adityathakur19200@gmail.com>
2025-05-05 22:40:45 +0530

    extra

--- client/src/components/config/api-url.ts
+++ client/src/components/config/api-url.ts
@@ -1,3 +1,3 @@
-// const API_URL =  "https://uniportalbackend.onrender.com"
-const API_URL = "http://localhost:3010/api"
+const API_URL =  "https://uniportalbackend.onrender.com/api"
+//const API_URL = "http://localhost:3010/api"
 export default API_URL;
